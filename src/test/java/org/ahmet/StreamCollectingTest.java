54a88f0f25372c178b93f7a3b986996407cfffa7
drahmetacik <a.acik@icloud.com>
2025-04-03 21:23:37 +0100

    Add unit test for collecting user data into a map in StreamCollectingTest class

--- src/test/java/org/ahmet/StreamCollectingTest.java
+++ src/test/java/org/ahmet/StreamCollectingTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -51,5 +52,16 @@ public class StreamCollectingTest {
         );
         Set<String> collectedUsersToSet = users.stream().map(User::getEmail).collect(Collectors.toSet());
         assertEquals(Set.of("alice@example.com", "bob@example.com", "charlie@example.com"), collectedUsersToSet);
+    }
+
+    @Test
+    public void testCollectToMap() {
+        List<User> users = Arrays.asList(
+                new User("Alice", 30, "alice@example.com", LocalDate.of(1993, 1, 1), null),
+                new User("Bob", 20, "bob@example.com", LocalDate.of(2003, 1, 1), null),
+                new User("Charlie", 25, "charlie@example.com", LocalDate.of(1998, 1, 1), null)
+        );
+        Map<String, Integer> userAgeMap = users.stream().collect(Collectors.toMap(User::getName, User::getAge));
+        assertEquals(Map.of("Alice", 30, "Bob", 20, "Charlie", 25), userAgeMap);
     }
 }
