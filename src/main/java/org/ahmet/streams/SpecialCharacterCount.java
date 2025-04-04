e40d073f79b2d479771aa74faa3e0dc81dd67d64
drahmetacik <a.acik@icloud.com>
2025-04-04 15:38:53 +0100

    Add SpecialCharacterCount class to count special characters in a string

---
+++ src/main/java/org/ahmet/streams/SpecialCharacterCount.java
@@ -0,0 +1,46 @@
+package org.ahmet.streams;
+
+public class SpecialCharacterCount {
+
+    /**
+     * Counts the number of special characters in a given string.
+     *
+     * @param str the input string
+     * @return the count of special characters
+     * @throws IllegalArgumentException if the input string is null
+     */
+    public static int countSpecialCharacters(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("String cannot be null");
+        }
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Counts the number of special characters in a given string using streams.
+     *
+     * @param str the input string
+     * @return the count of special characters
+     * @throws IllegalArgumentException if the input string is null
+     */
+    public static int countSpecialCharactersStream(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("String cannot be null");
+        }
+        return (int) str.chars()
+                .filter(c -> !Character.isLetterOrDigit(c))
+                .count();
+    }
+
+    public static void main(String[] args) {
+        String str = "$-+(68then&-)/'@";
+        System.out.println("Number of special characters: " + countSpecialCharacters(str));
+        System.out.println("Number of special characters (stream): " + countSpecialCharactersStream(str));
+    }
+}
