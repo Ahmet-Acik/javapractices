38181e378c29e680c0c54f3fdaccd28dd6b5ed92
drahmetacik <a.acik@icloud.com>
2025-04-09 19:53:43 +0100

    Fix test for Optional.equals() to use assertNotEquals() and Optional.of() for non-null values

--- src/test/java/org/ahmet/OptionalImplementTest.java
+++ src/test/java/org/ahmet/OptionalImplementTest.java
@@ -177,9 +177,9 @@ public class OptionalImplementTest {
     public void testOptionalEqualsWithDifferentValues() {
         String value1 = "Hello";
         String value2 = "World";
-        Optional<String> optionalValue1 = Optional.ofNullable(value1);
-        Optional<String> optionalValue2 = Optional.ofNullable(value2);
-        assertFalse(optionalValue1.equals(optionalValue2));
+        Optional<String> optionalValue1 = Optional.of(value1);
+        Optional<String> optionalValue2 = Optional.of(value2);
+        assertNotEquals(optionalValue1, optionalValue2);
     }
 
 
